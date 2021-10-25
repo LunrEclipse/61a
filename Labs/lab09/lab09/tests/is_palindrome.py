ad5770fc84e1c58dc8fd772b0e09289fcc8f435b
Brendan Wong <35351983+LunrEclipse@users.noreply.github.com>
2021-10-25 15:23:07 -0700

    10/25
    Started Lab09

---
+++ Labs/lab09/lab09/tests/is_palindrome.py
@@ -0,0 +1,27 @@
+test = {
+  'name': 'is_palindrome',
+  'points': 0,
+  'suites': [
+    {
+      'cases': [
+        {
+          'answer': '283f29564937de236873fd9ad423ca2b',
+          'choices': [
+            'Constant',
+            'Logarithmic',
+            'Linear',
+            'Quadratic',
+            'Exponential',
+            'None of these'
+          ],
+          'hidden': False,
+          'locked': True,
+          'multiline': False,
+          'question': 'The is_palindrome function runs in ____ time in the length of its input.'
+        }
+      ],
+      'scored': False,
+      'type': 'concept'
+    }
+  ]
+}
